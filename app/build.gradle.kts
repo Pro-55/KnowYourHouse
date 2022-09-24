dc3f31710897cf7dc8f9fa661e85bc34ee7ddfea
Pranit Rane <pranitrane619@gmail.com>
2022-09-25 04:25:31 +0530

    upgrade: dependencies
    1. gradle
    2. kotlin
    3. hilt
    4. compose compiler
    5. core
    6. material 3
    7. activity

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,34 +52,31 @@ android {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.2.0"
+        kotlinCompilerExtensionVersion = "1.3.1"
     }
 }
 
 dependencies {
     // Core
-    implementation("androidx.core:core-ktx:1.8.0")
+    implementation("androidx.core:core-ktx:1.9.0")
 
     // Compose
     implementation("androidx.compose.ui:ui:1.2.1")
-    implementation("androidx.compose.material3:material3:1.0.0-beta01")
+    implementation("androidx.compose.material3:material3:1.0.0-beta03")
     implementation("androidx.compose.ui:ui-tooling-preview:1.2.1")
 
     // Lifecycle
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.5.1")
 
     // Activity
-    implementation("androidx.activity:activity-compose:1.5.1")
+    implementation("androidx.activity:activity-compose:1.6.0")
 
     // Accompanist
     implementation("com.google.accompanist:accompanist-systemuicontroller:0.25.1")
 
     // Hilt
-    implementation("com.google.dagger:hilt-android:2.40.1")
-    kapt("com.google.dagger:hilt-compiler:2.40.1")
-
-    // Hilt ViewModel
-    implementation("androidx.hilt:hilt-lifecycle-viewmodel:1.0.0-alpha03")
+    implementation("com.google.dagger:hilt-android:2.43.2")
+    kapt("com.google.dagger:hilt-compiler:2.43.2")
 
     // Test
     testImplementation("junit:junit:4.13.2")
